572ff1f2bc805edd321549903ee85be9f043f34e
damien-angioni <damien.angioni@laplateforme.io>
2023-01-02 12:14:41 +0100

    Jobs done
    Travail terminé

---
+++ jour01/pour_aller_plus_loin.py
@@ -0,0 +1,11 @@
+contient = ("Banane")
+contient_pas= ("Marquis")
+lettre=("e")
+if lettre in contient:
+    print("trouvé")
+else:
+    print("ya pas")
+if lettre in contient_pas:
+    print("trouvé")
+else:
+    print("ya pas")
